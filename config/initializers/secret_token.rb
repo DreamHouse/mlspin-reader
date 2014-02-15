0e02ca7884234c78a4989334be0c7dff04f6df5e
jmao <jmao@brightcove.com>
2014-02-15 17:40:20 -0500

    Initialize a rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mlspin::Application.config.secret_token = '089a01a94691ffc4b539e54fa3fd755fd872f188498fcb98e21a6f97d420c93e4c1ba969d0ede4cf7607ad9e1143f020fa8e47ab35bcd4eb58d0b199efdc8b27'
