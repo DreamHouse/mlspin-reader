723b0351683bfc5e8132e919ad6210605e9ef63e
DreamHouse <maojiayin+house@gmail.com>
2014-03-07 09:41:29 -0500

    Fix the multi element line when parsing a detail page.

--- app/workers/mlspin_crawler.rb
+++ app/workers/mlspin_crawler.rb
@@ -10,8 +10,160 @@ class MlspinCrawler
     get_first_page
     (2..@page_count).each { |page_index| get_one_page(page_index) }
   end
+
+  # get_details("9 Keeler Farm Lexington, MA", content)  
+  def get_page_details(addr, link)
+    # TODO: read content from link
+    content = File.read("spec/fixtures/one_house.html")
+    get_details(addr, content)
+  end
+  
+  def get_details(addr, content)
+    home = Home.find_by(addr: addr)
+    doc = Nokogiri::HTML(content)
+    doc.css('html body center table tbody tr td table tbody tr td table tbody tr td table tbody tr td').each do |element|
+      case element.text
+      when /Style:/
+        update_attribute(home, 'Style', element)
+      when /Total Rooms:/
+        update_attribute(home, 'Total Rooms', element)
+      when /Bedrooms:/
+        update_attribute(home, 'Bedrooms', element)
+      when /Bathrooms:/
+        update_attribute(home, 'Bathrooms', element)
+      when /Master Bath:/
+        update_attribute(home, 'Master Bath', element)
+      when /Fireplaces:/
+        update_attribute(home, 'Fireplaces', element)
+      when /Color:/
+        update_attribute(home, 'Color', element)
+      when /Grade School:/
+        update_attribute(home, 'Grade School', element)
+      when /Middle School:/
+        update_attribute(home, 'Middle School', element)
+      when /High School:/
+        update_attribute(home, 'High School', element)
+      when /Directions:/
+        update_attribute(home, 'Directions', element)
+        
+      ###### FEATURES #########
+      when /Appliances:/
+        update_attribute(home, 'Appliances', element)
+      when /Area Amenities:/
+        update_attribute(home, 'Area Amenities', element)
+      when /Basement:/
+        update_attribute(home, 'Basement', element)
+      when /Beach:/
+        update_attribute(home, 'Beach', element)
+      when /Construction:/
+        update_attribute(home, 'Construction', element)
+      when /Electric:/
+        update_attribute(home, 'Electric', element)
+      when /Energy Features:/
+        update_attribute(home, 'Energy Features', element)
+      when /Exterior:/
+        update_attribute(home, 'Exterior', element)
+      when /Exterior Features:/
+        update_attribute(home, 'Exterior Features', element)
+      when /Flooring:/
+        update_attribute(home, 'Flooring', element)
+      when /Foundation Size:/
+        update_attribute(home, 'Foundation Size', element)
+      when /Foundation Description:/
+        update_attribute(home, 'Foundation Description', element)
+      when /Hot Water:/
+        update_attribute(home, 'Hot Water', element)
+      when /Insulation:/
+        update_attribute(home, 'Insulation', element)
+      when /Interior Features:/
+        update_attribute(home, 'Interior Features', element)
+      when /Lot Description:/
+        update_attribute(home, 'Lot Description', element)
+      when /Road Type:/
+        update_attribute(home, 'Road Type', element)
+      when /Roof Material:/
+        update_attribute(home, 'Roof Material', element)
+      when /Sewer Utilities:/
+        update_attribute(home, 'Sewer Utilities', element)
+      when /Utility Connections:/
+        update_attribute(home, 'Utility Connections', element)
+      when /Water Utilities:/
+        update_attribute(home, 'Water Utilities', element)
+      when /Waterfront:/
+        update_attribute(home, 'Waterfront', element)
+      when /Water View:/
+        update_attribute(home, 'Water View', element)
+      
+      ####### Other Property Info ###########
+      when /Adult Community:/
+        update_attribute(home, 'Adult Community', element)
+      when /Disclosure Declaration:/
+        update_attribute(home, 'Disclosure Declaration', element)
+      when /Exclusions:/
+        update_attribute(home, 'Exclusions', element)
+      when /Facing Direction:/
+        update_attribute(home, 'Facing Direction', element)
+      when /Home Own Assn:/
+        update_attribute(home, 'Home Own Assn', element)
+      when /Lead Paint:/
+        update_attribute(home, 'Lead Paint', element)
+      when /UFFI:/
+        update_attribute(home, 'UFFI', element)
+      when /Warranty Features:/
+        update_attribute(home, 'Warranty Features', element)
+      when /Year Built:/
+        update_attribute(home, 'Year Built', element)
+      when /Source:/
+        update_attribute(home, 'Source', element)
+      when /Year Built Description:/
+        update_attribute(home, 'Year Built Description', element)
+      when /Year Round:/
+        update_attribute(home, 'Year Round', element)
+      when /Short Sale w\/Lndr.App.Req:/
+        update_attribute(home, 'Short Sale w/Lndr.App.Req', element)
+      when /Lender Owned:/
+        update_attribute(home, 'Water View', element)
+      when /Pin:/
+        update_attribute(home, 'Pin', element)
+      when /Assessed:/
+        update_attribute(home, 'Assessed', element)
+      
+      ####### Tax ###########
+      when /Tax:/
+        update_attribute(home, 'Tax', element)
+      when /Tax Year:/
+        update_attribute(home, 'Tax Year', element)
+      when /Book:/
+        update_attribute(home, 'Book', element)
+      when /Page:/
+        update_attribute(home, 'Page', element)
+      when /Cert:/
+        update_attribute(home, 'Cert', element)
+      when /Zoning Code:/
+        update_attribute(home, 'Zoning Code', element)
+      when /Map:/
+        # "Map: \n Block: \n Lot:".match(/(Map)(.*)(Block)(.*)(Lot)(.*)/m)
+        # "Map: \n Block: \n Lot:"
+        # 1:"Map"
+        #         2:": \n "
+        #         3:"Block"
+        #         4:": \n "
+        #         5:"Lot"
+        #         6:":"
+        update_attribute(home, 'Map', element)
+      end
+    end
+    home.save!
+  rescue => e
+    Rails.logger.error "Failed to update home due to #{e.message}"
+    Rails.logger.debug e.backtrace
+  end
   
 protected
+  def update_attribute(home, name, element)
+    home[name.downcase.gsub(/[\s\.\/]+/, "_").to_sym] = sanitize_str(element.text.gsub("#{name}:", ''))
+  end
+  
   def upsert(status, size, price, addr, date, time, link)
     if Home.where(addr: addr).count > 0
       Rails.logger.debug "Home #{addr} already exists"
