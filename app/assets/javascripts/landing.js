5f41277f23be77940fc31807a2c9d0371709d6e3
DreamHouse <maojiayin+house@gmail.com>
2014-04-06 22:43:32 -0400

    Add a couple of carousel in landing page.

---
+++ app/assets/javascripts/landing.js
@@ -0,0 +1,13 @@
+$(function() {
+  $('#new_house_carousel').carousel({
+    'interval': 3000
+  });
+  
+  $('#apt_carousel').carousel({
+    'interval': 5000
+  });
+  
+  $('#sell_carousel').carousel({
+    'interval': 4000
+  });
+})
