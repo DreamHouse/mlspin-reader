2feb6665ad0870c3c750ea76084c09934d5a5089
DreamHouse <maojiayin+house@gmail.com>
2014-06-01 18:45:48 -0400

    Search by town name.

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -11,8 +11,17 @@ class LandingController < ApplicationController
     min_price = params[:min_price].to_i * 10000
     max_price = params[:max_price].to_i * 10000
     town = params[:town]
+    bedroom_cnt = params[:bedroom_count]
+    restroom_cnt = params[:restroom_count]
 
-    @homes = Home.where(status: 'ACT').between(price: min_price..max_price).asc(:price).paginate(:page => params[:page], :per_page => 30)
+    home_query = Home.where(status: 'ACT').between(price: min_price..max_price).asc(:price)
+    
+    unless town.blank?
+      home_query = home_query.where(addr: /#{town}, MA/i)
+    end
+    
+    
+    @homes = home_query.paginate(:page => params[:page], :per_page => 30)
     render 'search_list', layout: "top_bar"
   end
 end
