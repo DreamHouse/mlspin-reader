c0ef258dd1cc36379bec21ad117d9a4bc550ab91
DreamHouse <maojiayin+house@gmail.com>
2014-06-29 12:15:16 -0400

    add more fields, i.e. author to the article model.

--- app/models/article.rb
+++ app/models/article.rb
@@ -8,6 +8,7 @@ class Article
   field :source_link, type: String
   field :publish_date, type: DateTime
   field :content, type: String
-
+  field :author, type: String # original author
+  
   belongs_to :user
 end
