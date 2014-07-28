9274cbd3f4b34e652080f0943adb9e79091ab6a9
DreamHouse <maojiayin+house@gmail.com>
2014-07-27 23:53:51 -0400

    Implement saving content from mercury editor.

--- app/assets/javascripts/mercury.js
+++ app/assets/javascripts/mercury.js
@@ -447,6 +447,16 @@ window.Mercury = {
   // ## Debug Mode
   //
   // Turning debug mode on will log events and other various things (using console.debug if available).
-  debug: false
+  debug: true,
 
+  onload: function() {
+      Mercury.on('ready', function() {
+        var link = $('#mercury_iframe').contents().find('#save_url');
+        Mercury.saveUrl = link.val(); 
+      });
+      
+      Mercury.on('saved', function() {
+        window.location.href = window.location.href.replace(/\/editor\//i, '/');
+      });
+  }
 };
--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -3,6 +3,25 @@ class Admin::ArticlesController < ApplicationController
     render layout: "admin"
   end
 
+  def create
+    article = Article.create!(params)
+    redirect_to '/editor' + edit_admin_article_path(article.id)
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+    
+    render layout: 'admin_editor'
+  end
+  
+  def update
+    doc = params[:content]
+    # {"content"=>{"title"=>{"type"=>"full", "data"=>{}, "value"=>"test3", "snippets"=>{}}, "content"=>{"type"=>"full", "data"=>{}, "value"=>"bb<p></p>", "snippets"=>{}}}
+    @article = Article.find(params[:id])
+    @article.update_attributes!(title: doc["title"]["value"], content: doc["content"]["value"])
+    render json: {"result" => "everything is good"}
+  end
+  
   def index
     # check permission
     @articles = Article.all
