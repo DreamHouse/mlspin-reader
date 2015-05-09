1320ff2e234c977dfca45fe2393baad38ed929d8
DreamHouse <maojiayin+house@gmail.com>
2015-05-09 12:51:14 -0400

    Add six groups of tags.

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,9 +14,13 @@ outer_tag = Tag.create!(name: "屋顶、外墙、地基",
   level: 1, weight: 2, vocabulary: "维护房产")
 inner_tag = Tag.create!(name: "室内维护", level: 1, weight: 3, vocabulary: "维护房产")
 furniture_tag = Tag.create!(name: "家具家电", level: 1, weight: 4, vocabulary: "维护房产")
+plumb_heat_tag = Tag.create!(name: "排水和取暖", level: 1, weight: 5, vocabulary: "维护房产")
+other_tag = Tag.create!(name: "其他", level: 1, weight: 6, vocabulary: "维护房产")
 
 garden_tag.add_child("种草和维护草坪")
 garden_tag.add_child("砍树、剪枝和种树")
+garden_tag.add_child("除虫除蚁")
+garden_tag.add_child("栅栏")
 garden_tag.add_child("游泳池、桑拿浴房和按摩池")
 
 outer_tag.add_child("浇筑和维护地基")
@@ -32,3 +36,20 @@ inner_tag.add_child("厨房装修") # add more child items, countertop, cabinet,
 inner_tag.add_child("卫生间")
 inner_tag.add_child("内饰设计与房间布置")
 inner_tag.add_child("墙")
+
+furniture_tag.add_child("电力系统")
+furniture_tag.add_child("家具选择与布置")
+furniture_tag.add_child("网络、电话和有线电视")
+furniture_tag.add_child("家庭影院")
+
+plumb_heat_tag.add_child("锅炉和暖气")
+plumb_heat_tag.add_child("中央空调系统")
+plumb_heat_tag.add_child("电取暖设备")
+plumb_heat_tag.add_child("疏通管道与更换龙头")
+plumb_heat_tag.add_child("排水系统")
+
+other_tag.add_child("保洁服务")
+other_tag.add_child("节能改进")
+other_tag.add_child("过冬准备")
+other_tag.add_child("老年人服务")
+
