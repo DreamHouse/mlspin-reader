53293b336ba141671c1a72d9832fb033409ffba6
DreamHouse <maojiayin+house@gmail.com>
2015-05-22 16:30:17 -0400

    Show tags in two levels in admin page.

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -3,9 +3,17 @@ class Admin::TagsController < ApplicationController
   
   def index
     if params[:v] == '1'
-      @tags = Tag.where(vocabulary: '维护房产')
+      tags = Tag.where(vocabulary: '维护房产').where(level: 1)
     else
-      @tags = Tag.all
+      tags = Tag.all.where(level: 1)
+    end
+    
+    @tags = tags.to_a.each do |tag|
+      tag.children = Tag.where(parent: tag).to_a
+    end
+    
+    @tags.each do |tag| 
+      Rails.logger.debug tag.children.count
     end
   end
   
