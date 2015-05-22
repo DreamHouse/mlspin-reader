a10385df22c342e64abaf89600b67f85c7b3d6a3
DreamHouse <maojiayin+house@gmail.com>
2015-05-22 17:58:22 -0400

    Add an admin page to add tags.

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -11,10 +11,27 @@ class Admin::TagsController < ApplicationController
     @tags = tags.to_a.each do |tag|
       tag.children = Tag.where(parent: tag).to_a
     end
-    
-    @tags.each do |tag| 
-      Rails.logger.debug tag.children.count
+  end
+  
+  def new
+    if params[:v] == '1'
+      @tags = Tag.where(vocabulary: '维护房产').where(level: 1)
+    else
+      @tags = Tag.all.where(level: 1)
     end
   end
   
+  def create
+    if params[:parent]
+      parent = Tag.where(id: params[:parent]).first
+      @new_tag = parent.add_child(params[:name], desc: params[:desc])
+    else
+      @new_tag = Tag.new(name: params[:name], desc: params[:desc])
+    end
+    if @new_tag.valid?
+      @new_tag.save!
+    else
+      @error = @new_tag.errors.full_messages
+    end
+  end
 end
